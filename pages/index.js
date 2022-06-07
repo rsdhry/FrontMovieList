7fcfbb203e8ad6b155c8e4f40a4885c4fd1448a6
rusdih <rsdhry@gmail.com>
2022-06-07 21:30:17 +0700

    front7juni

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Intro from '../components/Intro';
 import NavigationBar from '../components/Navigationbar';
+import { Container, Row,Col } from 'react-bootstrap';
 export default function Home() {
  return (
    <div className='htmlAll'>
@@ -9,12 +11,10 @@ export default function Home() {
        <meta name="description" content="Movie List" />
        <link rel="icon" href="/favicon.ico" />
      </Head>
-
-     <div className='myBG border'>
+     <div className='myBG'>
        <NavigationBar></NavigationBar>
+       <Intro></Intro>
      </div>
-    
-     
    </div>
  )
 }
