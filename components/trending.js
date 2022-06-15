a241b16add10ec4f432c56b982d72b285feb214c
rusdih <rsdhry@gmail.com>
2022-06-15 12:44:49 +0700

    front14juni

--- components/trending.js
+++ components/trending.js
@@ -7,7 +7,7 @@ const Trending = () => {
    <Row>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/avangers.jpg" alt="card image"  height="auto"></Image>
+         <Image src="/avangers.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>Avangers</Card.Text>
          <Card.Text>Bagus 3.6/4.0</Card.Text>
@@ -18,7 +18,7 @@ const Trending = () => {
        </Col>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/batman.jpg" alt="card image"  height="auto"></Image>
+         <Image src="/batman.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>Batman Returns</Card.Text>
          <Card.Text>Bagus 3.8/4.0</Card.Text>
@@ -28,7 +28,7 @@ const Trending = () => {
        </Col>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/battleship.jpg" alt="card image"  height="auto"></Image>
+         <Image src="/battleship.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>War On The Water</Card.Text>
          <Card.Text>Bagus 3.5/4.0</Card.Text>
@@ -38,7 +38,7 @@ const Trending = () => {
        </Col>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/bumblebee.jpg" alt="card image"  height="auto"></Image>
+         <Image src="/bumblebee.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>BumbleBee Story</Card.Text>
          <Card.Text>Keren 4.0/4.0</Card.Text>
@@ -48,7 +48,7 @@ const Trending = () => {
        </Col>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/captainamerica.jpg" alt="card image"  height="auto"></Image>
+         <Image src="/captainamerica.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>The Captain of America</Card.Text>
          <Card.Text>Keren 3.9/4.0</Card.Text>
@@ -58,7 +58,7 @@ const Trending = () => {
        </Col>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/harrypotter.jpg" alt="card image"  height="auto"></Image>
+         <Image src="/harrypotter.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>Harry Potter - Story of Magic</Card.Text>
          <Card.Text>Bagus 3.5/4.0</Card.Text>
@@ -68,7 +68,7 @@ const Trending = () => {
        </Col>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/hulk.jpg" alt="card image"  height="auto"></Image>
+         <Image src="/hulk.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>Hulk - Big Green Man</Card.Text>
          <Card.Text>Bagus 3.6/4.0</Card.Text>
@@ -78,7 +78,7 @@ const Trending = () => {
        </Col>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/ironman.jpg" alt="card image"  height="auto"></Image>
+         <Image src="/ironman.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>Ironman</Card.Text>
          <Card.Text>Keren 3.9/4.0</Card.Text>
@@ -88,7 +88,7 @@ const Trending = () => {
        </Col>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/ironman2.jpg" alt="card image"  height="auto"></Image>
+         <Image src="/ironman2.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>Ironman 2</Card.Text>
          <Card.Text>Bagus 3.7/4.0</Card.Text>
@@ -98,7 +98,7 @@ const Trending = () => {
        </Col>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/jumanji.jpg" alt="card image"  height="auto"></Image>
+         <Image src="/jumanji.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>Jumanji is The Secret Game</Card.Text>
          <Card.Text>Keren 4.0/4.0</Card.Text>
@@ -108,7 +108,7 @@ const Trending = () => {
        </Col>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/shark.jpg" alt="card image"  height="auto"></Image>
+         <Image src="/shark.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>Monster in The Water</Card.Text>
          <Card.Text>Bagus 3.5/4.0</Card.Text>
@@ -118,7 +118,7 @@ const Trending = () => {
        </Col>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/spiderman.jpg" alt="card image"  height="auto"></Image>
+         <Image src="/spiderman.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>Spiderman</Card.Text>
          <Card.Text>Bagus 3.5/4.0</Card.Text>
@@ -128,7 +128,7 @@ const Trending = () => {
        </Col>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/superman.jpg" alt="card image"  height="auto"></Image>
+         <Image src="/superman.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>Superman Returns</Card.Text>
          <Card.Text>Keren 4.0/4.0</Card.Text>
@@ -138,7 +138,7 @@ const Trending = () => {
        </Col>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/thor.jpg" alt="card image"  height="auto"></Image>
+         <Image src="/thor.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>THOR</Card.Text>
          <Card.Text>Bagus 3.5/4.0</Card.Text>
@@ -148,7 +148,7 @@ const Trending = () => {
        </Col>
        <Col md={4} className='movieWrapper'>
        <Card className='bg-transparent text-white text-center movieImage'>
-         <Image src="/transformersjpg" alt="card image"  height="auto"></Image>
+         <Image src="/transformers.jpeg" alt="card image"  height="400px"></Image>
          <div className='bg-dark p-2 m-1'>
          <Card.Text>Transformers - Dark of The Moon</Card.Text>
          <Card.Text>Bagus 3.5/4.0</Card.Text>
